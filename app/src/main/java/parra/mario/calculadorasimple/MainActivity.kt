c448985fd294504e2e09b9a536ade28e0630d247
Laboratorio 1821 <lv1821@itson.edu.mx>
2020-02-11 11:07:51 -0700

    Finish simple calculator

--- app/src/main/java/parra/mario/calculadorasimple/MainActivity.kt
+++ app/src/main/java/parra/mario/calculadorasimple/MainActivity.kt
@@ -2,6 +2,7 @@ package parra.mario.calculadorasimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +12,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_sumar.setOnClickListener{
-            suma()
+            operacion(1)
+        }
+        btn_restar.setOnClickListener{
+            operacion(2)
+        }
+        btn_mult.setOnClickListener{
+            operacion(3)
+        }
+        btn_dividir.setOnClickListener {
+            operacion(4)
         }
     }
 
-    fun suma(){
+    fun operacion(op: Int){
         var num1 = et_num1.text.toString().toDouble()
         var num2 = et_num2.text.toString().toDouble()
 
-        var suma = num1 + num2
+        var resp: Double = 0.0
 
-        tv_resp.setText("$suma")
+        when(op){
+            1 -> resp = num1 + num2
+            2 -> resp = num1 - num2
+            3 -> resp = num1 * num2
+            4 -> {
+                if (num2 != 0.0){
+                    resp = num1 / num2
+                }else{
+                    //Mensaje para el usuario
+                    Toast.makeText(this, "ERROR: División por cero", Toast.LENGTH_LONG).show()
+                }
+
+            }}
+
+        tv_resp.setText("$resp")
     }
 
 
