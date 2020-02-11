394b76929e3f5a1ab3a43dc25722f11864a7daf5
Laboratorio 1821 <lv1821@itson.edu.mx>
2020-02-11 10:23:12 -0700

    Initial commit

---
+++ app/src/main/java/parra/mario/calculadorasimple/MainActivity.kt
@@ -0,0 +1,12 @@
+package parra.mario.calculadorasimple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
